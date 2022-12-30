3721b0eeb5999d2c9e59b547a00e24017f3c794f
Chiaravalle04 <nicolas.chiaravalle@gmail.com>
2022-12-30 12:09:17 +0100

    Esercizio Completato

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rincorri-la-decina
+
+Scrivi un programma che stampi i numeri da 1 a 100 andando a capo ogni 10.
---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+for (let i = 0; i <= 100; i++) {
+
+    console.log(i);
+
+    if (i % 10 === 0) {
+
+        console.log('\n');
+
+    }
+
+}
